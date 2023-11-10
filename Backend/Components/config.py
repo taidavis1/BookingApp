2582c08844449ca71c71feea074b16e82d808d26
taidavis1 <73322981+taidavis1@users.noreply.github.com>
2023-11-10 02:11:53 -0800

    Update config.py

--- Backend/Components/config.py
+++ Backend/Components/config.py
@@ -2,15 +2,9 @@ from datetime import timedelta
 
 class Configuration:
     
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://tmstart4:123giadinh@74.208.246.165:3306/thuysalon" 
+    SQLALCHEMY_DATABASE_URI = "" 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = '123giadinh'
     JWT_COOKIE_SECURE = True
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)
     SECRET_KEY = '123giadinh'
-    MAIL_SERVER='smtp.gmail.com'
-    MAIL_PORT = 465
-    MAIL_USERNAME = 'tmstart4@gmail.com'
-    MAIL_PASSWORD = 'ppbkjcmhsviugxun'
-    MAIL_USE_TLS = False
-    MAIL_USE_SSL = True
